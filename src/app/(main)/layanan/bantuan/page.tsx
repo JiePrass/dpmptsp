72732b65255ee97e40d1cc75bf2a7fd54e448d48
HeiDarrz <heidararizki@gmail.com>
2026-01-08 15:40:20 +0700

    fix padding

--- src/app/(main)/layanan/bantuan/page.tsx
+++ src/app/(main)/layanan/bantuan/page.tsx
@@ -103,7 +103,7 @@ export default function BantuanSection() {
   };
 
   return (
-    <section className="w-full py-32 bg-background">
+    <section className="w-full py-24 bg-background">
       <div className="max-w-7xl mx-auto px-6">
         <div className="mb-12 mt-10">
           <Breadcrumb className="mb-4">
--- src/app/(main)/layanan/kunjungan/page.tsx
+++ src/app/(main)/layanan/kunjungan/page.tsx
@@ -68,7 +68,7 @@ export default function KunjunganSection() {
   };
 
   return (
-    <section className="w-full py-16 bg-background">
+    <section className="w-full py-24 bg-background">
       <div className="max-w-7xl mx-auto px-6">
         <div className="mb-12 mt-10">
           <Breadcrumb className="mb-4">
